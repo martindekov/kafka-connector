5314e3f8a7465d2be5b06a2a3de296321666de34
Martin Dekov (VMware) <mdekov@vmware.com>
2018-11-05 00:17:28 -0800

    Add travis CI to the repository
    Add travis.yml which will trigger the `build.sh` script
    to build the kafka-connector dockerfile also updated
    the script to use `$TRAVIS_TAG` instead of `latest`
    
    Signed-off-by: Martin Dekov (VMware) <mdekov@vmware.com>

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
-docker build -t functions/kafka-connector:latest .
+if [ -z "$TRAVIS_TAG" ]; then
+    TRAVIS_TAG="latest"
+fi
+
+docker build -t functions/kafka-connector:$TRAVIS_TAG .
 (cd setup && docker service rm kafka_connector ; docker stack deploy kafka -c connector-swarm.yml)
